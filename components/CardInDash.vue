5ccafb156dcef685acc909890500a9b2f02a96c6
DvOmar <pagepc07@gmail.com>
2025-02-02 19:44:03 +0300

    some update

---
+++ components/CardInDash.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="text-gray-600 body-font  mb-4 w-full ">
+    <div class="p-4 w-full">
+      <div class="border-2 border-gray-200 px-4 py-6 rounded-lg justify-items-center">
+        <slot></slot>
+        <h2 class="title-font font-medium text-3xl text-gray-900">{{content}}</h2>
+        <p class="leading-relaxed">{{title}} </p>
+      </div>
+    </div>
+  </section>
+
+</template>
+<script>
+export default {
+  props:{
+    title:{
+      type: String,
+    },
+    content:{
+      type: String,
+    },
+
+  }
+}
+</script>
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,58 +1,62 @@
 <template>
   <div>
-    <h1>Welcome to the Protected Page</h1>
-    <p>You are logged in and can access this page.</p>
-    <button @click="logout">Logout</button>
+    <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 ">
+      <div class="grid grid-cols-3 md:grid-cols-4 sm:grid-cols-1 gap-3 ">
+<!--        <CardInDash content="2.3K" title="view">-->
+<!--        <slot>-->
+<!--          <img class=" px-4 py-6 rounded-lg justify-items-center w-24 " src="~/assets/images/views.png" alt="view" />-->
+<!--        </slot>-->
+<!--      </CardInDash>-->
+      <CardInDash :content="numberOfMessages" title="Messages">
+        <slot>
+          <img class=" px-4 py-6 rounded-lg justify-items-center w-24 " src="~/assets/images/messages.png" alt="view" />
+        </slot>
+      </CardInDash>
+<!--        <CardInDash content="2.3K" title="total hour of learn">-->
+<!--        <slot>-->
+<!--          <img class=" px-4 py-6 rounded-lg justify-items-center w-24 " src="~/assets/images/views.png" alt="view" />-->
+<!--        </slot>-->
+<!--      </CardInDash>-->
+        <CardInDash :content="numberOfSkills" title="skills">
+        <slot>
+          <img class=" px-4 py-6 rounded-lg justify-items-center w-24 " src="~/assets/images/skills.png" alt="skills" />
+        </slot>
+      </CardInDash>
+      </div>
+
+      <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800 ">
+                <p class="text-2xl text-gray-400 dark:text-gray-500">
+                  <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
+                  </svg>
+                </p>
+              </div>
+
+            </div>
   </div>
 </template>
 
 <script>
+import api from "~/API/api.js";
+
+definePageMeta({
+  layout: 'dashboard'
+})
 export default {
-  async mounted() {
-    const token = localStorage.getItem('auth_token'); // Fetch the token from localStorage
-    if (!token) {
-      // Redirect to login page if token is not found
-      this.$router.push('/auth');
-    } else {
-      try {
-        // Optionally, validate the token with the server
-        const response = await fetch('http://127.0.0.1:8000/api/validate-token', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        if (!response.ok) {
-          throw new Error('Invalid token');
-        }
-      } catch (error) {
-        console.error('Token validation failed:', error);
-        // If token is invalid, redirect to login
-        this.$router.push('/auth');
-      }
+  data(){
+    return{
+      numberOfSkills:'',
+      numberOfMessages:'',
     }
   },
-  methods: {
-    async logout() {
-      const token = localStorage.getItem('auth_token');
-      if (token) {
-        try {
-          await fetch('http://127.0.0.1:8000/api/logout', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          });
-        } catch (error) {
-          console.error('Logout failed:', error);
-        }
-      }
-      // Clear token and redirect to login
-      localStorage.removeItem('auth_token');
-      this.$router.push('/auth');
-    },
+  async mounted() {
+    const numberOfSkills = await api.get('/api/dashboard/numberOfSkills');
+      this.numberOfSkills=await numberOfSkills.data
+    const numberOfMessages = await api.get('/api/dashboard/numberOfMessages');
+      this.numberOfMessages=await numberOfMessages.data
+    console.log(numberOfSkills)
+
   },
+
 };
 </script>
